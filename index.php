e880ce328c70e09c7dc979dd65b6e770abe51edf
borodkoleg <borodkoleg@gmail.com>
2023-10-10 00:41:47 +0300

    add seo meta tags, fix video block

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="Enhance Upper-Extremity Rehabilitation Results with InMotion Robotic Therapy Solutions">
+  <meta name="keywords" content="InMotion Rehabilitation A.I Therapy">
   <link rel="stylesheet" href="sass/bootstrap.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
